14fe94bb689ece6a8c7abbe49e74e61d692ff373
Justina Kuchta <44618164+jwkuchta@users.noreply.github.com>
2019-11-18 14:08:13 -0500

    Update api_communicator.rb

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -10,13 +10,11 @@ def get_character_movies_from_api(character_name)
   character_name = character_name.split.map {|c| c.capitalize}.join(" ")
   film_urls = response_hash["results"].select {|result| result["name"] == character_name}.map {|c| c["films"]}.flatten
   film_info = film_urls.map {|url| JSON.parse(RestClient.get(url))}
-  
-  #binding.pry
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
-  films.each {|film| p "#{film["title"]}"}
+  films.each {|film| puts "#{film["title"]}"}
 end
 
 def show_character_movies(character)
